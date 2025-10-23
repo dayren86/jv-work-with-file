1f5c80f62c7673fadc5de899d3551c79fe230cc8
unknown <dayren86@gmial.com>
2025-10-23 15:06:17 +0300

    workwithfile hw v1

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -1,7 +1,54 @@
 package core.basesyntax;
 
-public class WorkWithFile {
-    public void getStatistic(String fromFileName, String toFileName) {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.StandardOpenOption;
+import java.util.List;
 
+public class WorkWithFile {
+
+    public void getStatistic(String fromFileName, String toFileName) {
+        File file = new File(fromFileName);
+        List<String> strings;
+        int buy = 0;
+        int supply = 0;
+
+        try {
+            strings = Files.readAllLines(file.toPath());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String string : strings) {
+            String[] split = string.split(",");
+
+            if (split[0].charAt(0) == 'b') {
+                buy += Integer.parseInt(split[1]);
+            }
+            if (split[0].charAt(0) == 's') {
+                supply += Integer.parseInt(split[1]);
+            }
+        }
+
+        String[] result = {"supply," + supply + System.lineSeparator()
+                + "buy," + buy + System.lineSeparator()
+                + "result," + (supply - buy)};
+
+        File file1 = new File(toFileName);
+        try {
+            file1.createNewFile();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        for (String string : result) {
+            try {
+                Files.write(file1.toPath(), string.getBytes(), StandardOpenOption.APPEND);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+
+        }
     }
 }
