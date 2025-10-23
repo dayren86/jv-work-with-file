3e7693ed6153b7f40c4428a4db7a290106209293
unknown <dayren86@gmial.com>
2025-10-23 16:11:01 +0300

    workwithfile hw v3

--- src/main/java/core/basesyntax/WorkWithFile.java
+++ src/main/java/core/basesyntax/WorkWithFile.java
@@ -18,10 +18,10 @@ public class WorkWithFile {
         for (String string : strings) {
             String[] split = string.split(",");
 
-            if (split[0].charAt(0) == 'b') {
+            if (split[0].equals("buy")) {
                 buy += Integer.parseInt(split[1]);
             }
-            if (split[0].charAt(0) == 's') {
+            if (split[0].equals("supply")) {
                 supply += Integer.parseInt(split[1]);
             }
         }
@@ -30,23 +30,23 @@ public class WorkWithFile {
         writeDataToFile(toFileName, report);
     }
 
-    public StringBuilder createReport(int buy, int supply) {
+    private StringBuilder createReport(int buy, int supply) {
         return new StringBuilder().append("supply,").append(supply).append(System.lineSeparator())
                 .append("buy,").append(buy).append(System.lineSeparator())
                 .append("result,").append(supply - buy);
     }
 
-    public void writeDataToFile(String toFileName, StringBuilder stringBuilder) {
+    private void writeDataToFile(String toFileName, StringBuilder stringBuilder) {
         File file = new File(toFileName);
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(file))) {
             writer.append(stringBuilder);
         } catch (IOException e) {
-            throw new RuntimeException("Can't write" + toFileName, e);
+            throw new RuntimeException("Can't write data to file " + toFileName, e);
         }
     }
 
-    public List<String> readDataFromFile(String fromFileName) {
+    private List<String> readDataFromFile(String fromFileName) {
         List<String> strings;
 
         try {
